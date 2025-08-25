fbb0229abf9cedc59b0f7eaefcaa33858bff6255
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-08-25 23:07:37 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <AppTitle 
+      text="Как покорить юную кейпопессу?"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppTitle from './components/AppTitle.vue';
+</script>
+
+<style module>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="$style.titleWrap">
+    <h1 class="title"> {{ text }} </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  text: string;
+}
+
+defineProps<Props>();
+</script>
+
+<style module>
+.titleWrap {
+  max-width: 500px;
+  padding: 10px;
+  background-color: var(--color-white);
+  border-radius: 10px;
+}
+</style>
