dc293a1240b6875890972581f6da013d216c06d7
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-08-27 19:45:43 +0300

    добавила AppModal и функцию, открывающую модалку

--- src/App.vue
+++ src/App.vue
@@ -6,33 +6,52 @@
     <div :class="$style.buttonList">
       <AppButton 
         text="50% шанс стать ее чаги~а"
+        @click="openModal()"
       />
       <AppButton 
         text="100% шанс стать ее чаги~а"
       />
-      <MemberCard 
+      <AppModal :isOpen="isOpenModal" />
+      <!-- <MemberCard 
         v-for="member in Memberlist"
         :key="member.id"
-        :image="member.img.href"
+        :image="member.img"
         :member="member"
-      />
+      /> -->
     </div>
   </div>
 </template>
 
-<script setup >
+<script setup lang="ts">
 import AppTitle from '@/components/AppTitle.vue';
 import AppButton from '@/components/ui/AppButton.vue';
-import MemberCard from '@/components/ui/MemberCard.vue';
+// import MemberCard from '@/components/ui/MemberCard.vue';
+import { ref } from 'vue';
+import AppModal from '@/components/ui/AppModal.vue';
 
-const Memberlist = [
-  {id: 1, img: new URL("@/assets/img/leeminho.jpg", import.meta.url), text: 'ляляля', name: 'Ли Минхо'},
-  {id: 2, img: new URL("@/assets/img/HanJisung.jpg", import.meta.url), text: 'ляляля', name: 'Хан Джисон'},
-  {id: 3, img: new URL("@/assets/img/kimseungmin.jpg", import.meta.url), text: 'ляляля', name:'Ким Сынмин'},
-  {id: 4, img: new URL("@/assets/img/YangJeong-in.jpg", import.meta.url), text: 'ляляля', name:'Ян Чонин'},
-  {id: 5, img: new URL("@/assets/img/HwangHyunjin.jpg", import.meta.url), text: 'ляляля', name:'Хван Хенджин'},
-  {id: 6, img: new URL("@/assets/img/SeoChang-bin.jpg", import.meta.url), text: 'ляляля', name:'Со Чанбин'},
-];
+// import LeeMinho from '@/assets/img/leeminho.jpg';
+// import HanJisung from '@/assets/img/HanJisung.jpg';
+// import KimSeungmin from '@/assets/img/kimseungmin.jpg';
+// import YangJeongIn from '@/assets/img/YangJeong-in.jpg';
+// import HwangHyunjin from '@/assets/img/HwangHyunjin.jpg';
+// import SeoChangBin from '@/assets/img/SeoChang-bin.jpg';
+
+// const Memberlist = [
+//   {id: 1, img: LeeMinho, text: 'ляляля', name: 'Ли Минхо'},
+//   {id: 2, img: HanJisung, text: 'ляляля', name: 'Хан Джисон'},
+//   {id: 3, img: KimSeungmin, text: 'ляляля', name:'Ким Сынмин'},
+//   {id: 4, img: YangJeongIn, text: 'ляляля', name:'Ян Чонин'},
+//   {id: 5, img: HwangHyunjin, text: 'ляляля', name:'Хван Хенджин'},
+//   {id: 6, img: SeoChangBin, text: 'ляляля', name:'Со Чанбин'},
+// ];
+
+const isOpenModal = ref(false);
+
+const openModal = () => {
+  isOpenModal.value = true;
+  console.log(isOpenModal.value);
+  
+}
 
 </script>
 
