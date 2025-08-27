80b5d32b698fe6b4d76e18a66803379d4ea3a93c
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-08-27 18:22:02 +0300

    дополнила мемберлист и добавила изображения к ним

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,12 @@ import AppButton from '@/components/ui/AppButton.vue';
 import MemberCard from '@/components/ui/MemberCard.vue';
 
 const Memberlist = [
-  {id: 1, img: new URL("@/assets/img/leeminho.jpg", import.meta.url), text: 'ляляля'},
-  {id: 2, img: new URL("@/assets/img/kimseungmin.jpg", import.meta.url), text: 'ляляля'},
+  {id: 1, img: new URL("@/assets/img/leeminho.jpg", import.meta.url), text: 'ляляля', name: 'Ли Минхо'},
+  {id: 2, img: new URL("@/assets/img/HanJisung.jpg", import.meta.url), text: 'ляляля', name: 'Хан Джисон'},
+  {id: 3, img: new URL("@/assets/img/kimseungmin.jpg", import.meta.url), text: 'ляляля', name:'Ким Сынмин'},
+  {id: 4, img: new URL("@/assets/img/YangJeong-in.jpg", import.meta.url), text: 'ляляля', name:'Ян Чонин'},
+  {id: 5, img: new URL("@/assets/img/HwangHyunjin.jpg", import.meta.url), text: 'ляляля', name:'Хван Хенджин'},
+  {id: 6, img: new URL("@/assets/img/SeoChang-bin.jpg", import.meta.url), text: 'ляляля', name:'Со Чанбин'},
 ];
 const aaa =new URL("@/assets/img/kimseungmin.jpg", import.meta.url);
 console.log(aaa);
